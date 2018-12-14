e49afb3439d3395ebe6a2ed514b7ff01300efabd
NotPapaFranku <notpapafranku@gmail.com>
2018-12-14 01:05:56 -0600

    add basic structure for scrappers

---
+++ modules/reddit.js
@@ -0,0 +1,24 @@
+const $ = require("cheerio"),
+	  request = require("request-promise"); 
+
+class RedditScraper {
+
+	constructor(interval){
+		this.interval = interval;
+		this.url = "https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States";
+	}
+
+	start(){
+		console.log("starting the scrape for reddit.");
+		request(this.url).then(function(html){
+			//success!
+			const wikiUrls = [];
+			for (let i = 0; i < 45; i++) {
+			  wikiUrls.push($('big > a', html)[i].attribs.href);
+			}
+			console.log(wikiUrls);
+		  });
+	}
+}
+
+module.exports = RedditScraper;
--- scrape.js
+++ scrape.js
@@ -1,25 +1,10 @@
-const rp = require('request-promise'),
-      $ = require('cheerio'), 
-      url = 'https://en.wikipedia.org/wiki/George_Washington',
-      url2 = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
+const RedditScraper = require("./modules/reddit"),
+      PhScraper = require("./modules/ph");
 
-rp(url)
-.then(function(html) {
-  console.log($('.firstHeading', html).text());
-  console.log($('.bday', html).text());
-})
-.catch(function(err) {
-  //handle error
-});
+const rs = new RedditScraper(100),
+      ps = new PhScraper(100);
 
-rp(url2).then(function(html){
-  //success!
-  const wikiUrls = [];
-  for (let i = 0; i < 45; i++) {
-    wikiUrls.push($('big > a', html)[i].attribs.href);
-  }
-  console.log(wikiUrls);
-})
-.catch(function(err){
-  //handle error
-});
+
+rs.start();
+ps.start();
+
