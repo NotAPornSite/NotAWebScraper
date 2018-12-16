c3fca16759271c592ad84af176c0dc478ec3d609
NotPapaFranku <notpapafranku@gmail.com>
2018-12-16 08:35:00 -0600

    add basic video parsing --not really working

--- modules/reddit.js
+++ modules/reddit.js
@@ -7,7 +7,10 @@ class RedditScraper {
 	constructor(interval){
 		this.interval = interval;
 		this.subreddits = [];
-		this.images = [];
+		this.images = {}; //images in their categories
+		this.allImages = []; //all images found
+		this.videos = {};
+		this.allVideos = []; 
 		this.testPage = "<html><head></head><body>";
 	}
 
@@ -17,6 +20,7 @@ class RedditScraper {
 
 	save(){
 		fs.writeFile('./data/images.json', JSON.stringify(this.images));
+		fs.writeFile('./data/videos.json', JSON.stringify(this.videos));
 		fs.writeFile('./data/testPage.html', this.testPage);
 	}
 
@@ -25,6 +29,8 @@ class RedditScraper {
 		let promises = [];
 		this.loadSubReddits();
 		this.subreddits.forEach(link =>{ //loop through each subreddit
+			this.images[link] = [];
+			this.videos[link] = [];
 			let options = {
 				uri: link,
 				headers: {
@@ -35,15 +41,25 @@ class RedditScraper {
 				request(options).then(html=>{ //request the page
 	
 					let soup = new JSSoup(html),
-						imgs = soup.findAll("img");
-			
+						imgs = soup.findAll("img"),
+						videos = soup.findAll("source");
 					imgs.forEach((img)=>{
-						if(!this.images.includes(img.attrs["src"])){
-							this.images.push(img.attrs["src"]);
+						if(!this.allImages.includes(img.attrs["src"])){ //if not found in any of the categories then add it 
+							this.images[link].push(img.attrs["src"]);
+							this.allImages.push(img.attrs["src"]);
+							this.testPage += "<img src='"+img.attrs["src"]+"'/>";
 						}
-		
-						this.testPage += "<img src='"+img.attrs["src"]+"'/>";
 					});
+						
+						
+					videos.forEach(video =>{
+						if(!this.allVideos.includes(video.attrs["src"])){ //if not found in any of the categories then add it 
+							this.videos[link].push(video.attrs["src"]);
+							this.allVideos.push(video.attrs["src"]);
+							this.testPage += "<video><source src='"+video.attrs["src"]+"' type='video/mp4'></source></video>";
+						}
+					});
+
 					this.save();
 					res();
 				}).catch(err=>{
